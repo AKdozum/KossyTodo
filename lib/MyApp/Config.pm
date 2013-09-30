fbe344895b18df99ed0d4f980a9b14f6e42ab940
Yusuke Enomoto <yusuke.enomoto@dena.com>
2013-09-30 09:23:26 +0900

    db

---
+++ lib/MyApp/Config.pm
@@ -0,0 +1,34 @@
+package MyApp::Config;
+use strict;
+use warnings;
+use Config::ENV qw(PLACK_ENV), default => 'development';
+use File::Spec;
+use YAML::XS qw();
+
+sub base_dir($) {
+    my $path = shift;
+    $path =~ s!::!/!g;
+    if (my $libpath = $INC{"$path.pm"}) {
+        $libpath =~ s!\\!/!g; # win32
+        $libpath =~ s!(?:blib/)?lib/+$path\.pm$!!;
+        File::Spec->rel2abs($libpath || './');
+    } else {
+        File::Spec->rel2abs('./');
+    }
+}
+
+my $base;
+sub load_yaml {
+    $base ||= base_dir(__PACKAGE__);
+    my $path = File::Spec->catfile($base, 'config', @_);
+    return () unless -f $path;
+    return YAML::XS::LoadFile($path);
+}
+
+common +{ load_yaml('common.pl') };
+
+for my $env (qw/development test production/) {
+    config $env => load_yaml("$env.yaml");
+}
+
+1;
